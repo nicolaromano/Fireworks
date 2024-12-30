a194265cbcd40e94f8d24e2e5f47c10ce013138a
Nicola Romanò <nicola.romano@ed.ac.uk>
2024-12-30 22:37:08 +0000

    plotting of center of firework

--- fireworks.sh
+++ fireworks.sh
@@ -1,6 +1,42 @@
 #!/usr/bin/env bash
 
-cat<<"EOF"
+# Create an array of colors to use with tput
 
-"Happy New Year!"
+colors=(
+    "$(tput setaf 33)" # blue
+    "$(tput setaf 190)" # yellow
+    "$(tput setaf 196)" # red
+    "$(tput setaf 255)" # white
+)
+
+ncolors=${#colors[@]}
+
+# configure terminal for drawing
+cleanup() {
+	tput rmcup # restore screen
+	tput cnorm # restore cursor
+}
+trap cleanup exit # run cleanup on exit
+
+tput smcup # save screen
+tput civis # hide cursor
+
+ncols=$(tput cols)
+nrows=$(tput lines)
+
+while true; do
+    # Choose a random x position for the firework
+    xpos=$((RANDOM % ncols))
+    ypos=$((RANDOM % nrows))    
+
+    tput cup $ypos $xpos
+    echo -n "${colors[$RANDOM % $ncolors]}*"
+
+    sleep 0.4
+
+    # Clear the screen
+    tput clear
+done
+
+
 EOF
