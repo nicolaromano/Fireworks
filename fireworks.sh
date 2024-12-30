51193e47f5813125d4c2aab2c75af73da331982d
Nicola Romanò <nicola.romano@ed.ac.uk>
2024-12-30 23:19:21 +0000

    Explosions!

--- fireworks.sh
+++ fireworks.sh
@@ -16,7 +16,7 @@ cleanup() {
 	tput rmcup # restore screen
 	tput cnorm # restore cursor
 }
-trap cleanup exit # run cleanup on exit
+trap cleanup EXIT # run cleanup on exit
 
 tput smcup # save screen
 tput civis # hide cursor
@@ -27,14 +27,50 @@ nrows=$(tput lines)
 while true; do
     # Choose a random x position for the firework
     xpos=$((RANDOM % ncols))
-    ypos=$((RANDOM % nrows))    
+    ypos=$((RANDOM % nrows))
+    firework_color=${colors[$RANDOM % ncolors]}
 
+    nframes=$((RANDOM % 5 + 5)) # between 5 and 9 frames
+
+    # ypos can't be less than nframes or more than nrows - nframes
+    ypos=$((ypos < nframes ? nframes : ypos))
+    ypos=$((ypos > nrows - nframes ? nrows - nframes : ypos))
+    # xpos can't be less than nframes or more than ncols - nframes
+    xpos=$((xpos < nframes ? nframes : xpos))
+    xpos=$((xpos > ncols - nframes ? ncols - nframes : xpos))
+
+    # Draw the firework
+    for j in $(seq 1 5); do
+        tput cup $((ypos + 5 - j)) $xpos
+        echo -n "${firework_color}ǁ"
+        sleep 0.1
+    done
     tput cup $ypos $xpos
-    echo -n "${colors[$RANDOM % $ncolors]}*"
+    echo -n "${firework_color}*"        
 
-    sleep 0.4
+    for j in $(seq 1 5); do
+        tput cup $((ypos + 6 - j)) $xpos
+        echo -n "${firework_color} "
+    done
 
+    for frame in $(seq 1 $nframes); do
+        for i in $(seq 1 $frame); do
+            tput cup $((ypos - i)) $((xpos - i))            
+            echo -n "${firework_color}\\"
+            tput cup $((ypos - i)) $((xpos + i))
+            echo -n "${firework_color}/"
+            tput cup $((ypos + i)) $((xpos - i))
+            echo -n "${firework_color}/"
+            tput cup $((ypos + i)) $((xpos + i))
+            echo -n "${firework_color}\\"
+            tput cup $ypos $((xpos - i))
+            echo -n "${firework_color}-"
+            tput cup $ypos $((xpos + i))
+            echo -n "${firework_color}-"
+            sleep 0.06
+        done
     # Clear the screen
+    done
     tput clear
 done
 
